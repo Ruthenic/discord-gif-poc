385a2127eeac5d25cbd6a61d3757af5026ec3bc7
Ruthenic <mdrakea3@tutanota.com>
2021-11-24 08:30:44 +0000

    make website prettier

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from quart import Quart, request, send_file
+from quart import Quart, request, send_file, render_template
 
 app = Quart(__name__)
 
@@ -11,7 +11,7 @@ async def POC():
     userAgent = request.headers.get('User-Agent')
     print('[log]: useragent ' + userAgent + ' requested gif.')
     if ('Firefox/' in userAgent or 'Chrome/' in userAgent) and not userAgent == 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0':
-        return "<p>There could've been a payload here, where I sent your data off to some random server.</p>"
+        return await render_template('oops.html')
     else:
         return await send_file('actualGif.gif')
 
@@ -20,7 +20,7 @@ async def POCSFW():
     userAgent = request.headers.get('User-Agent')
     print('[log]: useragent ' + userAgent + ' requested gif.')
     if ('Firefox/' in userAgent or 'Chrome/' in userAgent) and not userAgent == 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0':
-        return "<p>There could've been a payload here, where I sent your data off to some random server.</p>"
+        return await render_template('oops.html')
     else:
         return await send_file('actualSfwGif.gif')
 
